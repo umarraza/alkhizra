75fe4a18ad783a6a6f7438ddf02c81280d1d57dd
Umar Raza <umarraza@stackcru.com>
2019-06-13 18:24:53 +0500

    gdshg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,12 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
                     You are logged in!
                 </div>
             </div>
@@ -21,48 +19,44 @@
     </div>
 </div>
 
-    <div class="row">
-        <div class="col-lg-4 col-md-4"></div>
-
-        <div class="col-lg-4 col-md-4">
-            <div id="messageDiv"></div> <br>
-            <input type="hidden" id="userId" value="{{Auth::User()->id}}">
-            <input id="nameInput" class="form-control form-control-sm" type="text" placeholder="Name">
-            <input id="messageInput" class="form-control form-control-sm" type="text" placeholder="Message">
-        </div>
-
-        <div class="col-lg-4 col-md-4"></div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"></div>
+    <div class="col-lg-4 col-md-4">
+        <div id="messageDiv"></div> <br>
+        <input type="hidden" id="userId" value="{{Auth::User()->id}}">
+        <input type="hidden" id="nameInput" class="form-control form-control-sm" type="text" placeholder="Name" value="{{Auth::User()->name}}">
+        <p>{{Auth::User()->name}}</p>
+        <input id="messageInput" class="form-control form-control-sm" type="text" placeholder="Message">
     </div>
+    <div class="col-lg-4 col-md-4"></div>
+</div>
 
-    {{--  <input id="nameInput" placeholder="Name"/>
-    <input id="messageInput" placeholder="Message..."/>  --}}
+<script>
 
-    <script>
+    var messageRef = new Firebase('https://alkhizra-76467.firebaseio.com/');
 
-        var messageRef = new Firebase('https://alkhizra-76467.firebaseio.com/');
+    $('#messageInput').keypress(function(e){
+
+        if(e.keyCode == 13) {
+
+            var name = $('#nameInput').val();
+            var text = $('#messageInput').val();
+            var userId = $('#userId').val();
+
+            messageRef.push({name:name, text:text, userId:userId});
+
+            $('#messageInput').val();
+        }
+
+    }); 
+
+    messageRef.on('child_added',function(snapshot){
         
-        $('#messageInput').keypress(function(e){
+        var message = snapshot.val();
+        document.getElementById('messageDiv').innerHTML += message.name+': '+message.text+'<br/>';
 
-            if(e.keyCode == 13) {
+    });
 
-                var name = $('#nameInput').val();
-                var text = $('#messageInput').val();
-                var userId = $('#userId').val();
-
-                messageRef.push({name:name, text:text, userId:userId});
-
-                $('#messageInput').val();
-            }
-
-        }); 
-
-        messageRef.on('child_added',function(snapshot){
-            
-            var message = snapshot.val();
-            document.getElementById('messageDiv').innerHTML += message.name+': '+message.text+'<br/>';
-
-        });
-
-    </script>
+</script>
 
 @endsection
