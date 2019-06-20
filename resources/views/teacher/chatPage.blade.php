b77ebfbd2e18813c6b6f92b39647623b6d77b2eb
Umar Raza <umarraza2200@gmail.com>
2019-06-20 01:02:28 -0700

    chat

--- resources/views/teacher/chatPage.blade.php
+++ resources/views/teacher/chatPage.blade.php
@@ -4,11 +4,12 @@
 
 <div class="row">
     <div class="col-lg-4 col-md-4"></div>
-    <div class="col-lg-4 col-md-4">
+    <div class="col-lg-4 col-md-4 parent">
 
-        <div id="messageDiv1" class="pull-left"></div> <br>
-        <div id="messageDiv2" class="pull-right"></div> <br>
         
+        <div class="show-message"></div>
+
+
         <input type="hidden" id="userId" value="{{Auth::User()->id}}">
         <input type="hidden" id="nameInput" class="form-control form-control-sm" type="text" value="{{Auth::User()->name}}">
         <input type="hidden" id="roleId" class="form-control form-control-sm" type="text" value="{{Auth::User()->roleId}}">
@@ -54,11 +55,39 @@
 
             if (message.roleId == 2) {
 
-                document.getElementById('messageDiv1').innerHTML += message.name+': '+message.text+'<br/>';
+                // <div class="student-message">Umar Raza: Hello students?</div>
+                
+
+                var element = document.createElement('div');
+
+                element.className = `teacher-message`;
+                element.style.cssText = "width:400px;height:30px; background:#EBEDEF;margin:5px 0 5px 0;border-radius: 10px 40px 30px;"
+
+                element.appendChild(document.createTextNode(message.name+': '+message.text));
+
+
+                
+
+                const target = document.querySelector('.show-message');  
+
+                var parent = target.parentNode;
+
+                parent.insertBefore(element, target);
 
             } else if(message.roleId == 3) {
 
-                document.getElementById('messageDiv2').innerHTML += message.name+': '+message.text+'<br/>';
+                var element2 = document.createElement('div');
+                element2.className = `student-message `;
+
+                element2.style.cssText = "width:400px;height:30px; background:#F6DDCC;margin:5px 0 5px 200px;border-radius: 10px 40px 30px;"
+
+                element2.appendChild(document.createTextNode(message.name+': '+message.text));
+
+                const target = document.querySelector('.show-message');  
+
+                var parent = target.parentNode;
+
+                parent.insertBefore(element2, target);
 
             }
         }
